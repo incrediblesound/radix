f87846457f4a679e904267830678aa7301ee28cd
James H. Edwards <james.howe.edwards@gmail.com>
2014-08-03 16:17:22 -0700

    add radix-document

---
+++ radix-document.js
@@ -0,0 +1,205 @@
+// this is the main constructor function, each node takes a string 
+// as a value and has an array of children
+
+var Radix = function(value){
+  this.children = [];
+  this.locations = [];
+  if(value === undefined){
+    this.value = '';
+  } else {
+    this.value = value.string;
+    this.locations.push(value.index);
+  }
+};
+
+// the insert function inserts a new string into the tree
+
+Radix.prototype.insert = function(obj){
+  // if the string is already in the tree, return the node containing that string
+  if (this.value === obj.string) { 
+    this.locations.push(obj.index); 
+  }
+
+  var node, tst;
+  // otherwise iterate through the child nodes
+  for(var i = 0; i < this.children.length; i++) {
+
+    // store the current child and a regex containing its value
+    node = this.children[i];
+    tst = new RegExp('^'+node.value+'\.*');
+    // if the current child node's string is found in the input value, start over from that node
+    if(tst.test(obj.string)){
+      var l = node.value.length;
+      // we only pass on the difference between our input and the current node's value
+      obj.string = obj.string.substring(l, obj.string.length);
+      return node.insert(obj);
+    }
+  }
+  // if we don't find that value itself or a node containing part of it, 
+  // insert the string at the current node
+  this.children.push(new Radix(obj));
+};
+
+// this is a simple function that counts the number of characters stored in the entire
+// tree, can be used to calculate compression rate.
+Radix.prototype.countChars = function(){
+  var count = this.value.length;
+  if(this.children.length){
+    var node;
+    for(var i = 0; i < this.children.length; i++){
+      node = this.children[i];
+      count += node.countChars();
+    }
+  }
+  return count;
+};
+
+// check to see if a given string is in the
+Radix.prototype.contains = function(target, current){
+  if (current === undefined) {
+    current = this.value;
+  } 
+  var node, chk, newVal;
+  for(var i = 0; i < this.children.length; i++) {
+    node = this.children[i];
+    newVal = current + node.value;
+    if( newVal === target) { return true; }
+
+    chk = new RegExp('^'+newVal+'\.*');
+    if(chk.test(target)) {
+      return node.contains(target, newVal);
+    }
+  }
+  return false;
+};
+
+// this function constructs a list of all the words stored in the tree
+Radix.prototype.reconstruct = function(value){
+  var result = [];
+
+  var search = function(context, value){
+    console.log(context);
+    if (value !== undefined){
+    value = value + context.value;
+    } else {
+      value = context.value;
+    }
+    if (value.length){
+      for(var k = 0; k < context.locations.length; k++){
+        result[context.locations[k]] = value;
+      }
+    }
+    if (context.children.length){
+      var node;
+      for(var i = 0; i < context.children.length; i++){
+        node = context.children[i];
+        search(node, value);
+      }
+    }
+  }
+  search(this, value);
+  return result.join(' ');
+};
+
+// This is the classic autocomplete function. Given a partial word, the tree returns
+// a list of all the words that can be completed with that partial.
+Radix.prototype.complete = function(target, previous){
+  var result = [], current;
+  if(current === undefined){
+    current = this.value;
+  }
+  if(previous){
+    current = previous + this.value;   
+  }
+  for(var i = 0; i < this.children.length; i++){
+      var node = this.children[i];
+      var chk = new RegExp('^'+current+node.value+'\.*');
+      var tst = new RegExp('^'+target+'\.*');
+      if(chk.test(target)){
+        return node.complete(target, current);
+      }
+      else if(tst.test(current+node.value)){
+        result = result.concat(node.reconstruct(current));
+      }
+  }
+  return result;
+};
+
+Radix.prototype.sortedInsert = function(array){
+
+  var sort = function(left, right) {
+    var result = [];
+    while(left.length && right.length){
+      if(left[0].length <= right[0].length){
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+    while(left.length){
+      result.push(left.shift());
+    }
+    while(right.length){
+      result.push(right.shift());
+    }
+    return result;
+  };
+
+  var merge = function(array){
+    if(array.length < 2) {
+      return array;
+    }
+    var mid = Math.floor(array.length/2);
+    var left = array.slice(0, mid);
+    var right = array.slice(mid, array.length);
+    return sort(merge(left), merge(right));
+  };
+
+  var sorted = merge(array);
+  console.log(sorted);
+  for(var i = 0; i < sorted.length; i++){
+    this.insert(sorted[i]);
+  }
+};
+
+Radix.prototype.documentInsert = function(string){
+
+  var array = string.split(' ');
+  for(var k = 0; k < array.length; k ++) {
+    array[k] = { string: array[k], index: k };
+  }
+
+  var sort = function(left, right) {
+    var result = [];
+    while(left.length && right.length){
+      if(left[0].string.length <= right[0].string.length){
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+    while(left.length){
+      result.push(left.shift());
+    }
+    while(right.length){
+      result.push(right.shift());
+    }
+    return result;
+  };
+
+  var merge = function(array){
+    if(array.length < 2) {
+      return array;
+    }
+    var mid = Math.floor(array.length/2);
+    var left = array.slice(0, mid);
+    var right = array.slice(mid, array.length);
+    return sort(merge(left), merge(right));
+  };
+
+  var sorted = merge(array);
+  console.log(sorted);
+  for(var i = 0; i < sorted.length; i++){
+    this.insert(sorted[i]);
+  }
+};
